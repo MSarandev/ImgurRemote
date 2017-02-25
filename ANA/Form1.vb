5975a4d439ecda18f98e3730d202afcff0f9e53f
MSarandev <maxsarandev@gmail.com>
2017-02-25 13:54:49 +0000

    Full initial upload

---
+++ ANA/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            AppActivate("chrome")
+            My.Computer.Keyboard.SendKeys("{+}")
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Try
+            AppActivate("chrome")
+            My.Computer.Keyboard.SendKeys("{LEFT}")
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            AppActivate("chrome")
+            My.Computer.Keyboard.SendKeys("{RIGHT}")
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            AppActivate("chrome")
+            My.Computer.Keyboard.SendKeys("{-}")
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ ANA/My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ANA")> 
+<Assembly: AssemblyDescription("Imgur Remote Control")> 
+<Assembly: AssemblyCompany("M. Sarandev")> 
+<Assembly: AssemblyProduct("ANA")> 
+<Assembly: AssemblyCopyright("Copyright ©  2027")> 
+<Assembly: AssemblyTrademark("S.I.T.S")> 
+
+<Assembly: ComVisible(True)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7f8fbcab-e337-4d15-8615-6c346f88d9ce")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("en")> 
